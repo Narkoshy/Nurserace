9adb863b957eb0c32d9c9d7a4bdadbd0b37bc95e
Narkoshy <lacasadeana@gmail.com>
2025-02-17 14:49:00 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Grupo from './Grupo';
-import CarreraCaballos from './CarreraCaballos';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Grupo from "./Grupo";
+import CarreraCaballos from "./CarreraCaballos";
+import Login from "./components/Login"; // Importamos la pantalla de login
+import ProtectedRoute from "./components/ProtectedRoute"; // Importamos la ruta protegida
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<h1>Bienvenido al Juego de Preguntas</h1>} />
-                <Route path="/grupo1" element={<Grupo grupo="grupo1" />} />
-                <Route path="/grupo2" element={<Grupo grupo="grupo2" />} />
-                <Route path="/grupo3" element={<Grupo grupo="grupo3" />} />
-                <Route path="/carrera" element={<CarreraCaballos />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route 
+          path="/grupo1" 
+          element={<ProtectedRoute><Grupo grupo="grupo1" /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/grupo2" 
+          element={<ProtectedRoute><Grupo grupo="grupo2" /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/grupo3" 
+          element={<ProtectedRoute><Grupo grupo="grupo3" /></ProtectedRoute>} 
+        />
+        <Route 
+          path="/carrera" 
+          element={<ProtectedRoute><CarreraCaballos /></ProtectedRoute>} 
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
