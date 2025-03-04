8a8296e0ded4615fc6fcea33d6db8ce00fac2684
Sergio Garcia Redondo <sergiogarciaredondo@MacBook-Air-de-Sergio.local>
2025-03-04 19:05:44 +0100

    afegir 20 preguntes

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,82 @@ app.use(cors());
 app.use(express.json());
 
 const preguntas = [
-    { pregunta: "¿Cuál es la capital de Francia?", opciones: ["Madrid", "París", "Berlín", "Roma"], correcta: 1 },
-    { pregunta: "¿Cuánto es 2+2?", opciones: ["3", "4", "5", "6"], correcta: 1 },
-    { pregunta: "¿Quién fue el descubridor de las vacunas?", opciones: ["Edward Jener", "Louis Pasteur", "Alexander Flemming", "John Snow"], correcta: 0 },
-    { pregunta: "¿Cuándo se inició la vacunación COVID en España?", opciones: ["Febrero 2021", "Septiembre 2020", "Diciembre 2019", "Diciembre 2020"], correcta: 3 },
+    { 
+        pregunta: "Qui va descobrir les vacunes?", 
+        opciones: ["Edward Jener", "Louis Pasteur", "Alexander Fleming", "Cristina i Sara"], 
+        correcta: 0 
+    },
+    { 
+        pregunta: "A quina edat s’administra la vacuna TV?", 
+        opciones: ["12 mesos – 3 anys", "15 mesos – 3 anys", "9 mesos – 3 anys", "dosis única als 15 mesos"], 
+        correcta: 0 
+    },
+    { 
+        pregunta: "A quina temperatura s’han de mantenir les vacunes a la nevera?", 
+        opciones: ["entre 3ºC - 8 ºC", "no importa la temperatura", "entre 2ºC - 8ºC", "entre 2ºC – 10ºC"], 
+        correcta: 2 
+    },
+    { 
+        pregunta: "Quants tipus de vacuna hi ha?", 
+        opciones: ["vacunes microorganismes sencers, de subunitats, d’ADN", "vacunes microorganismes sencers, de subunitats, d’ADN i toxoide", "vacunes vives-mortes, vives-bacterianes", "vacunes atenuades-inactivades, víriques i bacterianes"], 
+        correcta: 1 
+    },
+    { 
+        pregunta: "Acut a la consulta pacient de 90 anys per vacunar-se de la vacuna de la grip. Actualment, només tenim al centre la vacuna de la grip adjuvada. Veient el seu historial vacunal, fa una setmana que la infermera d’atdom, li ha administrat la primera dosi de vacuna HZ. Què hem de fer?", 
+        opciones: ["Reprogramem visita a les 3 setmanes, per administrar-la conjuntament amb HZ", "Es pot administrar la vacuna de la grip", "Si administrem la vacuna de la grip, la vacuna HZ perd eficàcia", "Reprogramem visita en dos dies que arriben les vacunes de la grip no adjuvada"], 
+        correcta: 1 
+    },
+    { 
+        pregunta: "Quan s’inicia la campanya de vacunació gripal?", 
+        opciones: ["Setembre", "Sempre podem vacunar de la grip", "Octubre", "Novembre"], 
+        correcta: 2 
+    },
+    { 
+        pregunta: "Pacient de 18 anys que li van administrar 1era dosi de vacuna de la varicel·la el 19/10/2024 i el 14/11/2024 per error li van administrar una dosi de vacuna shingrix. Davant aquest error, quina pauta s’ha de seguir?", 
+        opciones: ["Iniciar nova pauta de varicel·la, ja que no es comptabiltza com administrada", "Administrar 1 dosi de varicel·la, per completar pauta", "Administrar 1 dosi de varicel·la i 1 dosi de shingrix per completar pauta", "Demanar analítica per valorar anticossos de la varicel·la, per si fa falta completar pauta de varicel·la, ja que recorda haver patit la malaltia."], 
+        correcta: 3 
+    },
+    { 
+        pregunta: "Assenyala la resposta incorrecta respecte al virus de l’hepatitis B (VHB)", 
+        opciones: ["VHB pot sobreviure fora de l'organisme almenys 7 dies i pot causar infecció si penetra en un altre organisme en aquest període", "L'hepatitis B és prevenible amb la vacuna actualment disponible", "El VHB es transmet pels aliments o aigües contaminades", "La infecció crònica pel virus de l'hepatitis B pot tractar-se amb medicaments, en particular amb agents antivirals orals"], 
+        correcta: 2 
+    },
+    { 
+        pregunta: "Quina és la via d’administració més utilitzada per administrar les vacunes?", 
+        opciones: ["SC", "intradèrmica", "oral", "IM"], 
+        correcta: 3 
+    },
+    { 
+        pregunta: "A quin any es va instaurar el primer calendari sistemàtic a Espanya?", 
+        opciones: ["1981", "1968", "1975", "1970"], 
+        correcta: 1 
+    },
+    { 
+        pregunta: "En una escola bressol han detectat un cas de xarampió. Acut a la consulta un nen de 9 mesos per ser vacunat segons indicació de Salut pública, que fem?", 
+        opciones: ["No podem administrar, doncs seguint calendari, la vacuna de la TV s’administra als 12 mesos", "Li administrem la vacuna TV, i caldrà seguir pauta segons calendari", "Li administrem la vacuna TV, avançant la dels 12 mesos, i aquesta queda registrada com a primera dosi.", "Abans d’administrar, pregunto a la referent de vacunes del centre"], 
+        correcta: 1 
+    },
+    { 
+        pregunta: "Quan es va iniciar la vacunació del COVID a Espanya?", 
+        opciones: ["Febrer 2021", "Setembre 2021", "Desembre 2019", "Desembre 2020"], 
+        correcta: 3 
+    },
+    { 
+        pregunta: "Acut adolescent de 17 anys, sense factors de risc, per administrar la vacuna VPH9.", 
+        opciones: ["Li administrem 1 dosis, la té finançada", "Administrem 1 dosis, no la té finançada", "Administrem 2 dosis, les té finançades (0-5 mesos)", "Administrem 3 dosis, les té finançades (0-2-6 mesos)"], 
+        correcta: 2 
+    },
+    { 
+        pregunta: "La sèrie d'elements i activitats necessàries per a garantir la potència immunitzadora de les vacunes des de la seva fabricació fins a l'administració d'aquestes a la població, es defineix com:", 
+        opciones: ["Cadena del fred", "Cadena de transport", "Transport actiu", "Conservació positiva"], 
+        correcta: 0 
+    },
+    { 
+        pregunta: "Quines dues vacunes no es poden posar juntes?", 
+        opciones: ["Varicel·la i triple vírica", "Varicel·la i febre groga", "Triple vírica i febre groga", "Totes es poden administrar conjuntament"], 
+        correcta: 2 
+    }
+
 ];
 
 let progresoGrupos = { grupo1: 0, grupo2: 0, grupo3: 0 };
