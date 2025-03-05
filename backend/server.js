824faa1d7b56fc971b4dc0840a81048446be0a6f
Sergio Garcia Redondo <sergiogarciaredondo@MacBook-Air-de-Sergio.local>
2025-03-05 14:51:15 +0100

    Canvi preguntes

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ const preguntas = [
     { 
         pregunta: "Pacient de 18 anys que li van administrar 1era dosi de vacuna de la varicel·la el 19/10/2024 i el 14/11/2024 per error li van administrar una dosi de vacuna shingrix. Davant aquest error, quina pauta s’ha de seguir?", 
         opciones: ["Iniciar nova pauta de varicel·la, ja que no es comptabiltza com administrada", "Administrar 1 dosi de varicel·la, per completar pauta", "Administrar 1 dosi de varicel·la i 1 dosi de shingrix per completar pauta", "Demanar analítica per valorar anticossos de la varicel·la, per si fa falta completar pauta de varicel·la, ja que recorda haver patit la malaltia."], 
-        correcta: 3 
+        correcta: 1 
     },
     { 
         pregunta: "Assenyala la resposta incorrecta respecte al virus de l’hepatitis B (VHB)", 
@@ -59,7 +59,7 @@ const preguntas = [
     { 
         pregunta: "A quin any es va instaurar el primer calendari sistemàtic a Espanya?", 
         opciones: ["1981", "1968", "1975", "1970"], 
-        correcta: 1 
+        correcta: 2 
     },
     { 
         pregunta: "En una escola bressol han detectat un cas de xarampió. Acut a la consulta un nen de 9 mesos per ser vacunat segons indicació de Salut pública, que fem?", 
@@ -74,7 +74,7 @@ const preguntas = [
     { 
         pregunta: "Acut adolescent de 17 anys, sense factors de risc, per administrar la vacuna VPH9.", 
         opciones: ["Li administrem 1 dosis, la té finançada", "Administrem 1 dosis, no la té finançada", "Administrem 2 dosis, les té finançades (0-5 mesos)", "Administrem 3 dosis, les té finançades (0-2-6 mesos)"], 
-        correcta: 2 
+        correcta: 0 
     },
     { 
         pregunta: "La sèrie d'elements i activitats necessàries per a garantir la potència immunitzadora de les vacunes des de la seva fabricació fins a l'administració d'aquestes a la població, es defineix com:", 
@@ -85,6 +85,56 @@ const preguntas = [
         pregunta: "Quines dues vacunes no es poden posar juntes?", 
         opciones: ["Varicel·la i triple vírica", "Varicel·la i febre groga", "Triple vírica i febre groga", "Totes es poden administrar conjuntament"], 
         correcta: 2 
+    },
+ { 
+        pregunta: "Nen de 19 anys que acut a consulta per seguir calendari accelerat de vacunacions. No ha passat la varicel·la. Segons nota infermera avui cal administrar VVZ i TV. Revisant calendari, fa 15 dies li van administrar VPH9 + TV i també té pendent administrar meningitis i HA. Quines vacunes cal administrar?", 
+        opciones: [
+            "Administrem TV + VVZ, segons nota infermera", 
+            "Administrem VPH9 + MACWY", 
+            "Administrem HA + MACWY", 
+            "Reprogramem visita amb la seva infermera"
+        ], 
+        correcta: 2 
+    },
+    { 
+        pregunta: "Acut pacient de 36 anys per realització de PAPPS. Al preguntar sobre riscs d’ITS, comenta que manté relacions amb homes. Expliquem vacunació del papil·loma i accepta administració de la vacuna.", 
+        opciones: [
+            "Administrem una dosi, ja que la té finançada", 
+            "Administrem 3 dosis amb pauta: 0-6-12 mesos, ja que la té finançada", 
+            "Administrem 2 dosis amb pauta 0-6, ja que la té finançada", 
+            "No administrem, ja que no està finançada per l’edat que té. Li recomanem que la compri. Pauta a seguir: 3 dosis 0-1-6 mesos"
+        ], 
+        correcta: 2 
+    },
+    { 
+        pregunta: "Acut a la consulta adolescent de 16 anys, doncs a la seva escola hi ha un brot de tos ferina. Revisem calendari història clínica i veiem que li falta la Td dels 14 anys. La seva mare ens diu que fa un any que va patir la tosferina. Què fem?", 
+        opciones: [
+            "Vacunem 1 dosi de Td, per seguir calendari.", 
+            "No administrem cap vacuna, ja que ha patit la tos ferina.", 
+            "Vacunem 1 dosi de Dtpa", 
+            "Vacunem 1 dosi de Td i recitem per administrar una dosi de VPH9."
+        ], 
+        correcta: 2 
+    },
+    { 
+        pregunta: "Respecte la vacuna antipneumocòcia, és cert que?", 
+        opciones: [
+            "Interval entre Pn23 i Pn20 és de 6m", 
+            "Interval entre Pn13 i Pn20 és de 12m", 
+            "Interval entre Pn20 i Pn23 és de 6 m", 
+            "Interval entre Pn23 i Pn20 és de 12m"
+        ], 
+        correcta: 3 
+    },
+    { 
+        pregunta: "Acut a la consulta una mare de 50 anys explicant que a l’escola del seu fill hi ha un brot de xarampió. Què fem?", 
+        opciones: [
+            "Administrem 1 dosi de TV i citem al mes per administrar la 2º dosi.", 
+            "Demanem analítica per mirar anticossos", 
+            "No farem res, ja que per edat segur que ha passat la malaltia", 
+            "Administrem 1 dosi de TV"
+        ], 
+        correcta: 0 
     }
 
 ];
